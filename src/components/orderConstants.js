43799fc238c89801beab27489b4bbd946de87fb5
Rekha Yadav <149398442+rekhayd@users.noreply.github.com>
2025-08-08 10:08:25 +0530

    Delete src directory

--- src/components/orderConstants.js
+++
@@ -1,9 +0,0 @@
-
-export const headers = [
-  { label: "No", key: "No" },
-  { label: "Email", key: "Email" },
-  { label: "Total", key: "Total" },
-  { label: "Status", key: "Status" },
-  { label: "Date", key: "Date" },
-  { label: "Items", key: "Items" }
-];
